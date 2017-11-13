d149762799b375a08837ccf13032dfd1a6d3e861
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2017-11-13 17:29:46 -0500

    mobile responsive nav bar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
-	document.addEventListener("turbolinks:load", function(){
+document.addEventListener("turbolinks:load", function(){
 
-  	const elNav = document.querySelector(".nav-bar-options");
-  	const elHam = document.querySelector("#nav-icon3");
+	const elNav = document.querySelector(".nav-bar-options");
+	const elHam = document.querySelector("#nav-icon3");
 
-  	elHam.addEventListener("click", function(){
-        elNav.classList.toggle("open");
-    });
+	elHam.addEventListener("click", function(){
+    elNav.classList.toggle("open");
   });
+});
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy, :vote]
   respond_to :js, :json, :html
-  before_action :authenticate_restaurant!, except: [:live_restaurants]
+  before_action :authenticate_restaurant!, except: [:live_restaurants, :all_restaurants]
 
   # GET /restaurants
   # GET /restaurants.json
@@ -82,7 +82,9 @@ class RestaurantsController < ApplicationController
   def live_restaurants
     @restaurants = Restaurant.all
     @discounts = Discount.live
-    
+  end
+  def all_restaurants
+    @restaurants = Restaurant.all
   end
 
   def choosing_option
